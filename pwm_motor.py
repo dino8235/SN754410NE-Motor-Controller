0420817b09a023fa51be186d1a57cdefbc913ceb
Charlie <80426428+dino8235@users.noreply.github.com>
2022-07-23 21:18:00 -0300

    Update pwm_motor.py
    added parentheses on prints
    organised spacing in loop
    "except:" -> "except KeyboardInterrupt:"

--- pwm_motor.py
+++ pwm_motor.py
@@ -20,29 +20,36 @@ m2b = GPIO.PWM(M2_B, 200)
 
 try:
     while True:
-        print "Forward 50%"
+        
+        print ("Forward 50%")
         m1f.start(50)
         m2f.start(50)
         sleep(2)
-        print "Forward 100%"
+        
+        print ("Forward 100%")
         m1f.ChangeDutyCycle(100)
         m2f.ChangeDutyCycle(100)
         sleep(2)
-        print "Stop"
+        
+        print ("Stop")
         m1f.stop()
         m2f.stop()
         sleep(1)
-        print "Backward 50%"
+        
+        print ("Backward 50%")
         m1b.start(50)
         m2b.start(50)
         sleep(2)
-        print "Backwards 100%"
+        
+        print ("Backwards 100%")
         m1b.ChangeDutyCycle(100)
         m2b.ChangeDutyCycle(100)
         sleep(2)
-        print "M1 Stop"
+        
+        print ("M1 Stop")
         m1b.stop()
         m2b.stop()
         sleep(1)
-except:
+        
+except KeyboardInterrupt:
     GPIO.cleanup()
